fe12e92d00b5cc0b6349ed302cca178b98d4eb7d
Evan <suzukaze.hazuki2020@gmail.com>
2022-02-21 18:56:23 +0900

    feat: mui admin

---
+++ flutter-tools-admin/src/contexts/SidebarContext.tsx
@@ -0,0 +1,18 @@
+import React, { FC, useState, createContext } from 'react';
+
+// type SidebarContextType = { sidebarToggle: any; toggleSidebar: () => void };
+
+export const SidebarContext = createContext({});
+
+export const SidebarProvider: FC = ({children}) => {
+	const [sidebarToggle, setSidebarToggle] = useState(false);
+	const toggleSidebar = () => {
+		setSidebarToggle(!sidebarToggle);
+	};
+
+	return (
+		<SidebarContext.Provider value={{sidebarToggle, toggleSidebar}}>
+			{children}
+		</SidebarContext.Provider>
+	);
+};
